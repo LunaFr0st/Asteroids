b03624ab13ccf1b8968962378abb7a34196ef14a
LunaFr0st <codyamies@gmail.com>
2017-08-10 16:06:31 +1000

    Actually Added the Breakout folder structure etc.

---
+++ Assets/4-Loop&Arrays/Scripts/Loops.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace LoopsArrays
+{
+    public class Loops : MonoBehaviour
+    {
+        public string msg = "You got Mail";
+        public float frequency = 6f;
+        public float amplitude = 12f;
+
+
+
+        private float timer = 0;
+        public float printTime = 2f;
+
+        public GameObject spawnPrefab;
+        public float spawnRadius = 5f;
+        public int enemyCount = 10;
+
+        public GameObject[] spawnPrefabs;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            SpawnObjectsWithSine();
+        }
+
+        void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, spawnRadius);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer <= printTime)
+            {
+                print(msg);
+            }
+        }
+        void SpawnObjectsWithSine()
+        {
+            for (int i = 0; i < enemyCount; i++)
+            {
+                int randomIndex = Random.Range(0, spawnPrefabs.Length);
+
+                float x = Mathf.Sin(i * frequency) * amplitude;               //float x = Mathf.Sin(Mathf.Cos(Mathf.Tan(i * Time.deltaTime))) * enemyCount;
+                float y = i;
+                float z = Mathf.Cos(i * frequency) * amplitude;
+                GameObject randomPrefab = spawnPrefabs[randomIndex];
+                GameObject clone = Instantiate(randomPrefab);
+
+                MeshRenderer rend = clone.GetComponent<MeshRenderer>();
+                float r = Random.Range(0, 2);
+                float g = Random.Range(0, 2);
+                float b = Random.Range(0, 2);
+
+                rend.material.color = new Color(r, g, b);
+
+                Vector3 randomPos = transform.position + new Vector3(x, y, z); //Random.insideUnitSphere * spawnRadius;
+                clone.transform.position = randomPos;
+
+            }
+        }
+    }
+}
+
---
+++ Assets/~Breakout/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Breakout
+{
+    public class GameManager : MonoBehaviour
+    {
+        public int width = 20;
+        public int height = 20;
+        public GameObject[] blockPrefabs;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            GenerateBlocks();
+        }
+
+        // Function with Arguments
+      /*  GameObject GetBlockByIndex(int index)
+        {
+            if (index > blockPrefabs.Length || index < 0)
+            {
+                return null;
+            }
+            GameObject clone = Instantiate(blockPrefabs[index]);
+            return clone;
+        }*/
+
+        GameObject GetRandomBlock()
+        {
+            int randomIndex = Random.Range(0, blockPrefabs.Length);
+            GameObject randomPrefab = blockPrefabs[randomIndex];
+            GameObject clone = Instantiate(randomPrefab);
+            return clone;
+            
+        }
+        void GenerateBlocks()
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    GameObject block = GetRandomBlock();
+                    Vector3 pos = new Vector3(x, y, 0);
+                    block.transform.position = pos;
+                }
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
