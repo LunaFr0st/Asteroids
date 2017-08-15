2b0a824b2290c656aeaa36c1d66a0837d829494d
LunaFr0st <codyamies@gmail.com>
2017-08-15 12:10:13 +1000

    Added new Breakout Features

---
+++ Assets/5-Arrays/Scripts/Weapon.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arrays
+{
+    public class Weapon : MonoBehaviour
+    {
+        public int damage = 10;
+        public int maxBullets = 30;
+        public float fireInterval = 0.2f;
+        public GameObject bulletPrefab;
+        public Transform spawnPoint;
+
+        private Transform target;
+        private bool isFired = false;
+        private int currentBullets = 0;
+        private Bullet[] spawnedBullets;
+        // Use this for initialization
+        void Start()
+        {
+            spawnedBullets = new Bullet[maxBullets];
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(!isFired && currentBullets < maxBullets)
+            {
+                StartCoroutine(Fire());
+            }
+        }
+
+        IEnumerator Fire()
+        {
+            isFired = true;
+            SpawnBullet();
+            yield return new WaitForSeconds(fireInterval);
+            isFired = false;
+        }
+        void SpawnBullet()
+        {
+            /*  1. Instantiate Bullet clone
+                2. Create direction varaible for bullet and rotating
+                3. Rotate the weapon to direction
+                4. Grab the bullet script from clone
+                5. Send bullet to target (by setting direction)
+                6. Store bullet in array using currentBullets as index
+                7. Increment currentBullets
+            */
+            GameObject clone = Instantiate(bulletPrefab, spawnPoint.position, Quaternion.identity); //Instantiate Bullet clone
+            Vector2 direction = target.position - transform.position;//Create direction varaible for bullet and rotating
+            direction.Normalize();//Create direction varaible for bullet and rotating
+            transform.rotation = Quaternion.LookRotation(direction);//Rotate the weapon to direction
+            Bullet bullet = clone.GetComponent<Bullet>();//Grab the bullet script from clone
+            bullet.direction = direction;//Send bullet to target (by setting direction)
+            spawnedBullets[currentBullets] = bullet;//Store bullet in array using currentBullets as index
+            currentBullets++;//Increment currentBullets
+        }
+        public void SetTarget(Transform target)
+        {
+            this.target = target;
+        }
+    }
+}
+
--- Assets/~Breakout/Scripts/GameManager.cs
+++ Assets/~Breakout/Scripts/GameManager.cs
@@ -9,7 +9,12 @@ namespace Breakout
         public int width = 20;
         public int height = 20;
         public GameObject[] blockPrefabs;
+        public Vector2 spacing = new Vector2(25f, 10f);
+        public Vector2 offset = new Vector2(-25f, 10f);
 
+        [Header("Debug")]
+        public bool isDebugging = false;
+        private GameObject[,] spawnedBlocks;
 
         // Use this for initialization
         void Start()
@@ -18,15 +23,15 @@ namespace Breakout
         }
 
         // Function with Arguments
-      /*  GameObject GetBlockByIndex(int index)
-        {
-            if (index > blockPrefabs.Length || index < 0)
-            {
-                return null;
-            }
-            GameObject clone = Instantiate(blockPrefabs[index]);
-            return clone;
-        }*/
+        /*  GameObject GetBlockByIndex(int index)
+          {
+              if (index > blockPrefabs.Length || index < 0)
+              {
+                  return null;
+              }
+              GameObject clone = Instantiate(blockPrefabs[index]);
+              return clone;
+          }*/
 
         GameObject GetRandomBlock()
         {
@@ -34,25 +39,45 @@ namespace Breakout
             GameObject randomPrefab = blockPrefabs[randomIndex];
             GameObject clone = Instantiate(randomPrefab);
             return clone;
-            
+
         }
         void GenerateBlocks()
         {
+            spawnedBlocks = new GameObject[height, width];
             for (int x = 0; x < width; x++)
             {
                 for (int y = 0; y < height; y++)
                 {
                     GameObject block = GetRandomBlock();
-                    Vector3 pos = new Vector3(x, y, 0);
+                    Vector2 pos = new Vector3(x * spacing.x, y * spacing.y);
                     block.transform.position = pos;
+                    spawnedBlocks[x, y] = block;
                 }
             }
         }
 
+        void UpdateBlocks()
+        {
+            //loop through entire 2d array
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    //update Positions
+                    GameObject currentBlock = spawnedBlocks[x, y];
+                    Vector2 pos = new Vector2(x * spacing.x, y * spacing.y);
+                    pos += offset;
+                    currentBlock.transform.position = pos;
+                }
+            }
+        }
         // Update is called once per frame
         void Update()
         {
-
+            if (isDebugging)
+            {
+                UpdateBlocks();
+            }
         }
     }
 }
