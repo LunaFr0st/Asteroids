25cf089f1f6851c1d67747a4a88764909a43bce4
LunaFr0st <codyamies@gmail.com>
2017-08-31 15:43:19 +1000

    Added Tower Defense Added Tower Defense Enemy AI Added Prefabs for Levels

---
+++ Assets/~TowerDefense/Scripts/EnemyAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace TowerDefense
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class EnemyAI : MonoBehaviour
+    {
+        public Transform target;
+        private NavMeshAgent nav;
+        // Use this for initialization
+        void Awake()
+        {
+            nav = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //If Target is not null
+                   //Set nav destenation to target position
+            if(target != null)
+            {
+                nav.SetDestination(target.position);
+            }
+        }
+    }
+}
+
