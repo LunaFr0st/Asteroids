91460688b84546fc6179513d9e388d0a5099a45b
LunaFr0st <codyamies@gmail.com>
2017-09-05 11:59:48 +1000

    Added Recursions

---
+++ Assets/7-Recursion/Scripts/RecursiveSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Recursion
+{
+    public class RecursiveSpawner : MonoBehaviour
+    {
+        public GameObject spawnPrefab;
+        public int amount = 100;
+        public float positionOffset = 10f;
+        [Range(0, 1)]
+        public float percentageReduction = 0.2f;
+        void Start()
+        {
+            Vector3 pos = transform.position;
+            Vector3 scale = spawnPrefab.transform.localScale;
+            RecursiveSpawn(amount, pos, scale);
+        }
+
+        void RecursiveSpawn(int currentAmount, Vector3 position, Vector3 scale)
+        {
+            Vector3 adjustedScale = scale * (1 - percentageReduction);
+            Vector3 adjustedPosition = position + Vector3.up * (adjustedScale.magnitude * positionOffset);
+            GameObject clone = Instantiate(spawnPrefab); // Instantiates the Prefab
+            clone.transform.position = adjustedPosition; // Sets the Position to the Adjusted Position
+            clone.transform.localScale = adjustedScale; // Sets the local scale to the adjusted scale
+
+            amount--; // Decrement
+            if(amount <= 0)
+            {
+                return;
+            }
+            RecursiveSpawn(currentAmount, adjustedPosition, adjustedScale);
+        }
+
+    }
+}
+
---
+++ Assets/7-Recursion/Scripts/ScaleOnY.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Recursion
+{
+    public class ScaleOnY : MonoBehaviour
+    {
+        public float maxY = 500f;
+        public float maxScale = 100f;
+        private float percentY = 0;
+        void Start()
+        {
+
+        }
+        void OnCollisionEnter(Collision col)
+        {
+
+        }
+        void Update()
+        {
+            Vector3 scale = transform.localScale;
+            Vector3 pos = transform.position;
+
+            float yPercent = pos.y / maxY;
+            float invertYPercent = 1 - yPercent;
+            float scaleFactor = maxScale * invertYPercent;
+            scale = Vector3.one * scaleFactor;
+            transform.localScale = scale;
+        }
+    }
+}
+
