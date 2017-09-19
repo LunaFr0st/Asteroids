6181143f116caef70e9ed51406f67063dfed4074
LunaFr0st <codyamies@gmail.com>
2017-09-19 12:09:40 +1000

    BallDrop added

---
+++ Assets/~AHundredBalls/Scripts/Paddle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace AHundredBalls
+{
+    public class Paddle : MonoBehaviour
+    {
+        public bool opened = false;
+        private Animator anim;
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                opened = true;
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                opened = false;
+            }
+        }
+        void UpdateAnimation()
+        {
+            anim.SetBool("IsOpen", opened);
+        }
+    }
+}
+
