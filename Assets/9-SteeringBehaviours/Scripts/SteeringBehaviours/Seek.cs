882991fc49d7976819037cb02a1be04454c5d60b
LunaFr0st <codyamies@gmail.com>
2017-10-12 16:06:30 +1100

    AI Added

---
+++ Assets/9-SteeringBehaviours/Scripts/SteeringBehaviours/Seek.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace AI
+{
+    public class Seek : SteeringBehaviour
+    {
+        public Transform target;
+        public float stoppingDistance = 1f;
+
+        public override Vector3 GetForce()
+        {
+            Vector3 force = Vector3.zero;
+            if(target == null)
+            {
+                return force;
+            }
+            Vector3 desiredForce = target.position - transform.position;
+            if (desiredForce.magnitude > stoppingDistance)
+            {
+                desiredForce = desiredForce.normalized * weight;
+                force = desiredForce - owner.velocity;
+            }
+            return force;
+        }
+    }
+}
+
